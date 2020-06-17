959b5a0ea034f0bd07935a1cf46c394d6caca84a
YERIM <dnaer456@gmail.com>
2020-06-17 15:02:59 +0900

    CSS add

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./styles/app.css";
 
 class App extends React.Component {
   constructor(pros) {
@@ -9,7 +10,8 @@ class App extends React.Component {
   render() {
     return (
       <div className="app">
-        <div className="app__header">Hi</div>
+        <div className="app__header"></div>
+        <div className="app__line"></div>
       </div>
     );
   }
