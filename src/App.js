fdf4e677c242125a2e81ef16fd2efacc9c9257f1
YERIM <dnaer456@gmail.com>
2020-06-25 21:59:13 +0900

    ADD CSS & BUTTON

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React from "react";
 import "./styles/app.css";
+import img1 from "./img/img1.jpg";
+import img2 from "./img/img2.jpg";
 
 class App extends React.Component {
   constructor(pros) {
     super(pros);
 
-    this.state = {};
+    this.state = { imgpath: "" };
   }
   render() {
+    const { imgpath } = this.state;
     return (
       <div className="app">
-        <div className="app__top">Test</div>
-        <div className="app__line"></div>
-        <div className="app__middle">Test1</div>
-        <div className="app__line"></div>
-        <div className="app__bottom">Test2</div>
-        <div className="app__line"></div>
+        <div className="app__top">제목</div>
+
+        <div className="app__middle">사진and버튼</div>
+        <div className="app__Btn">
+          <input type="button" value="photo"></input>
+        </div>
+
+        <div className="app__bottom">설명</div>
       </div>
     );
   }
