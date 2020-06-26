0bc4d3d26d3a058f655122bf2ee50246e9ebab4e
YERIM <dnaer456@gmail.com>
2020-06-26 22:40:11 +0900

    Fail

--- src/App.js
+++ src/App.js
@@ -12,18 +12,129 @@ class App extends React.Component {
     this.state = { imgpath: "", btnClick: false };
   }
   render() {
-    const { imgpath } = this.state;
+    const { imgpath, btnClick } = this.state;
     return (
       <div className="app">
         <div className="app__top">제목</div>
 
         <div className="app__middle">
           <div className="btn">
-            <input type="button" value="photo"></input>
+            <input
+              type="button"
+              value="photo"
+              onClick={this._potoHandler}
+            ></input>
+
+            <input
+              type="button"
+              value="photo1"
+              onClick={this._potoHandler1}
+            ></input>
+
+            <input
+              type="button"
+              value="photo2"
+              onClick={this._potoHandler2}
+            ></input>
+
+            <input
+              type="button"
+              value="photo3"
+              onClick={this._potoHandler3}
+            ></input>
+
+            <input
+              type="button"
+              value="photo4"
+              onClick={this._potoHandler4}
+            ></input>
           </div>
+        </div>
+
+        <div className="loding">
+          {btnClick ? <img src={imgpath} /> : <div>loding . . .</div>}
         </div>
       </div>
     );
   }
+  _potoHandler = () => {
+    this.setState({
+      btnClickFlag: false,
+    });
+
+    setTimeout(() => {
+      this.setState({
+        btnClick: true,
+      });
+    }, 2000);
+
+    this.setState({
+      imgpath: img1,
+    });
+  };
+
+  _potoHandler1 = () => {
+    this.setState({
+      btnClick: false,
+    });
+
+    setTimeout(() => {
+      this.setState({
+        btnClick: true,
+      });
+    }, 2000);
+
+    this.setState({
+      imgpath: img1,
+    });
+
+    _potoHandler2 = () => {
+      this.setState({
+        btnClickFlag: false,
+      });
+
+      setTimeout(() => {
+        this.setState({
+          btnClick: true,
+        });
+      }, 2000);
+
+      this.setState({
+        imgpath: img2,
+      });
+    };
+    _potoHandler3 = () => {
+      this.setState({
+        btnClickFlag: false,
+      });
+
+      setTimeout(() => {
+        this.setState({
+          btnClick: true,
+        });
+      }, 2000);
+
+      this.setState({
+        imgpath: img3,
+      });
+    };
+
+    _potoHandler4 = () => {
+      this.setState({
+        btnClickFlag: false,
+      });
+
+      setTimeout(() => {
+        this.setState({
+          btnClick: true,
+        });
+      }, 2000);
+
+      this.setState({
+        imgpath: img4,
+      });
+    };
+  };
 }
+
 export default App;
